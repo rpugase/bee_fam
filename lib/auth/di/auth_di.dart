9b8c60cb78a5639536244680d83db0eb1979cd54
andriih <rpugase@gmail.com>
2022-03-23 18:10:12 +0100

    Mock auth repo for simulator

--- lib/auth/di/auth_di.dart
+++ lib/auth/di/auth_di.dart
@@ -2,14 +2,18 @@ import 'package:birthday_gift/auth/data/firebase_auth_datastore.dart';
 import 'package:birthday_gift/auth/domain/auth_with_phone_number.dart';
 import 'package:birthday_gift/auth/domain/confirm_phone_number_code.dart';
 import 'package:birthday_gift/auth/presentation/auth_cubit.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
 
 final sl = GetIt.instance;
 
 Future<void> init() async {
+  bool isPhysicalDevice = (await DeviceInfoPlugin().deviceInfo).toMap()['isPhysicalDevice'] != "false";
+
   sl.registerSingleton(FirebaseAuth.instance);
-  sl.registerSingleton<FirebaseAuthDatastore>(FirebaseAuthDatastoreImpl(sl()));
+  sl.registerSingleton<FirebaseAuthDatastore>(
+      isPhysicalDevice ? FirebaseAuthDatastoreImpl(sl()) : FirebaseAuthDatastoreMock());
   sl.registerFactory(() => AuthWithPhoneNumber(sl()));
   sl.registerFactory(() => ConfirmPhoneNumberCode(sl(), sl()));
   sl.registerFactory(() => AuthCubit(sl(), sl()));
